781af5fa11b678325563e088687de3566534defa
Raafay76642 <mudassar76642@gmail.com>
2021-07-08 02:39:18 +0500

    notification are working now

--- app/src/main/java/com/example/fitnesszone3/MainActivity.java
+++ app/src/main/java/com/example/fitnesszone3/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+        FirebaseMessaging.getInstance().subscribeToTopic("FitnessZOne");
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
